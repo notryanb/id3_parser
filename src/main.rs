28e06a241450f5535560ba657d07299bcde547f2
notryanb <notryanb@gmail.com>
2018-09-09 11:13:15 -0400

    read ID and Version

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::io::Read;
 use std::path::PathBuf;
 
-
+// ID3v2.4.0 Docs: http://id3.org/id3v2.4.0-structure
 fn main() -> io::Result<()> {
     let mut mp3_path = PathBuf::from(r"/Users/RyanB/Desktop/mp3_id3_test_files");
     // mp3_path.push("02-cashout");
@@ -11,14 +11,51 @@ fn main() -> io::Result<()> {
     mp3_path.set_extension("mp3");
 
     let mut mp3_file = File::open(mp3_path).expect("Failed to open file");
-    let mut buffer = [0; 48];
+    // let mut buffer = [0; 10];
 
-    mp3_file.read(&mut buffer).expect("Failed to read buffer");
+    let mut tag = Tag::new();
+    tag.parse_header(&mut mp3_file);
+    // mp3_file.read(&mut tag.header).expect("Failed to read buffer");
 
-    for i in 0..buffer.len() {
-        println!("{}", buffer[i] as char);
+    for i in 0..tag.header.identifier.len() {
+        println!("{}", tag.header.identifier[i]);
     }
-
     Ok(())
 }
-// 73, 68, 51, 3, 0,0,0,0,15,109,84
+
+pub struct Tag {
+    pub header: Header,
+}
+
+impl Tag {
+    pub fn new() -> Tag {
+        Tag {
+            header: Header::new(),
+        }
+    }
+
+    pub fn parse_header(&mut self, file_path: &mut File) {
+       file_path.read(&mut self.header.identifier).expect("Can't read ID"); 
+       file_path.read(&mut self.header.version).expect("Can't read Version"); 
+    }
+}
+
+pub struct Header {
+    pub identifier: [u8; 3],
+    pub version: [u8; 2],
+    pub flags: [u8; 1], // Use bit masking to uncover which flags are set
+    pub size: [u8; 4],
+
+}
+
+impl Header {
+    fn new() -> Header {
+        Header {
+            identifier: [0; 3],
+            version: [0; 2],
+        }
+    }
+
+
+
+}
